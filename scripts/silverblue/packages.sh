05798be4c4e410b6c02e42b3b74d7eafff26a498
Emblem-66 <143352594+Emblem-66@users.noreply.github.com>
2024-11-07 17:43:50 +0100

    Create packages.sh

---
+++ scripts/silverblue/packages.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+rpmfusion=(
+    "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
+    "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
+)
+rpm -q rpmfusion-free-release rpmfusion-nonfree-release || rpm-ostree install ${rpmfusion[@]}
+
+install_packages=(
+	"adw-gtk3-theme"
+  "gnome-shell-extension-caffeine"
+  "ffmpegthumbnailer"
+)
+rpm -q ${install_packages[@]} || rpm-ostree install ${install_packages[@]}
+
+git clone https://github.com/mukul29/legacy-theme-auto-switcher-gnome-extension.git /usr/share/gnome-shell/extensions/legacyschemeautoswitcher@joshimukul29.gmail.com
