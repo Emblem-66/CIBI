63208f183f737192950fe24a11ad77041b051f2f
Emblem-66 <143352594+Emblem-66@users.noreply.github.com>
2024-11-08 18:18:57 +0100

    Update BuildSilverblue.sh

--- BuildSilverblue.sh
+++ BuildSilverblue.sh
@@ -22,6 +22,7 @@ curl -s https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts
 curl -s https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/system/mesa-git.sh | bash
 curl -s https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/system/codecs.sh | bash
 curl -s https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/system/fonts.sh | bash
+curl -s https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/system/utilities.sh | bash
 curl -s https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/system/just.sh | bash
 
 curl -s https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/silverblue/debloat.sh | bash
