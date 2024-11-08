4b893362f48b9100093542879ac753f575142ae3
Emblem-66 <143352594+Emblem-66@users.noreply.github.com>
2024-11-08 18:03:09 +0100

    Update codecs.sh

--- scripts/system/codecs.sh
+++ scripts/system/codecs.sh
@@ -9,26 +9,26 @@ rpmfusion=(
 rpm -q rpmfusion-free-release rpmfusion-nonfree-release || rpm-ostree install ${rpmfusion[@]}
 
 install_packages=(
-  "ffmpeg"
-	"gstreamer1-plugin-libav"
-	"gstreamer1-plugins-bad-free-extras"
-	"gstreamer1-plugins-bad-freeworld"
-	"gstreamer1-plugins-ugly"
-	"gstreamer1-vaapi"
-  "libavcodec-freeworld"
+"ffmpeg"
+"gstreamer1-plugin-libav"
+"gstreamer1-plugins-bad-free-extras"
+"gstreamer1-plugins-bad-freeworld"
+"gstreamer1-plugins-ugly"
+"gstreamer1-vaapi"
+"libavcodec-freeworld"
 )
 
 remove_packages=(
-  "noopenh264"
-  "ffmpeg-free"
-	"libavcodec-free"
-	"libavdevice-free"
-	"libavfilter-free"
-	"libavformat-free"
-	"libavutil-free"
-	"libpostproc-free"
-	"libswresample-free"
-	"libswscale-free"
+"noopenh264"
+"ffmpeg-free"
+"libavcodec-free"
+"libavdevice-free"
+"libavfilter-free"
+"libavformat-free"
+"libavutil-free"
+"libpostproc-free"
+"libswresample-free"
+"libswscale-free"
 )
 
 rpm-ostree uninstall ${remove_packages[@]}
