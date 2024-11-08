3d39584e1baeb384bb71976d8330d56bc8892a72
Emblem-66 <143352594+Emblem-66@users.noreply.github.com>
2024-11-08 18:18:12 +0100

    Create utilities.sh

---
+++ scripts/system/utilities.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+rpmfusion=(
+    "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
+    "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
+)
+rpm -q rpmfusion-free-release rpmfusion-nonfree-release || rpm-ostree install ${rpmfusion[@]}
+
+install_packages=(
+"corectrl"
+)
+
+remove_packages=(
+""
+)
+
+#rpm-ostree uninstall ${remove_packages[@]}
+rpm-ostree install ${install_packages[@]}
+
