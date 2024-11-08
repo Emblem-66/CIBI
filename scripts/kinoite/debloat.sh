5d3017dece0f4ecb0117ade8e51e0f6e2c26b346
Emblem-66 <143352594+Emblem-66@users.noreply.github.com>
2024-11-08 23:06:40 +0100

    Update debloat.sh

--- scripts/kinoite/debloat.sh
+++ scripts/kinoite/debloat.sh
@@ -3,9 +3,15 @@
 set -ouex pipefail
 
 remove_packages=(
-    "toolbox"
-    "firefox"
-    "firefox-langpacks"
+"toolbox"
+"firefox"
+"firefox-langpacks"
+"kf5-kwallet"
+"kf5-kwallet-libs"
+"kf6-kwallet"
+"kf6-kwallet-libs"
+"kwalletmanager5"
+"pam-kwallet"
 )
 
 rpm -q ${remove_packages[@]} || rpm-ostree uninstall ${remove_packages[@]}
