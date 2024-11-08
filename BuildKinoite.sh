e69fd1d940497b954606091c1438cb067f35f90d
Emblem-66 <143352594+Emblem-66@users.noreply.github.com>
2024-11-08 18:18:45 +0100

    Update BuildKinoite.sh

--- BuildKinoite.sh
+++ BuildKinoite.sh
@@ -20,6 +20,7 @@ curl -s https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts
 curl -s https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/system/mesa-git.sh | bash
 curl -s https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/system/codecs.sh | bash
 curl -s https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/system/fonts.sh | bash
+curl -s https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/system/utilities.sh | bash
 curl -s https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/system/just.sh | bash
 
 curl -s https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/kinoite/debloat.sh | bash
