0ffa8f09e15de40ba07f8225278e1fb01aed4d96
Emblem-66 <143352594+Emblem-66@users.noreply.github.com>
2024-11-14 22:28:46 +0100

    Create tailscale.sh

---
+++ scripts/system/tailscale.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+curl -o /etc/yum.repos.d/_tailscale.repo "https://pkgs.tailscale.com/stable/fedora/tailscale.repo"
+
+install_packages=(
+"tailscale"
+)
+
+remove_packages=(
+""
+)
+
+#rpm-ostree uninstall ${remove_packages[@]}
+rpm-ostree install ${install_packages[@]}
+
+systemctl enable tailscaled
