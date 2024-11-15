97792692f9ae1d7a7936d41489872147c68603df
Emblem-66 <143352594+Emblem-66@users.noreply.github.com>
2024-11-15 19:35:56 +0100

    Create distrobox.sh

---
+++ scripts/system/distrobox.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+rpmfusion=(
+    "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
+    "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
+)
+#rpm -q rpmfusion-free-release rpmfusion-nonfree-release || rpm-ostree install ${rpmfusion[@]}
+
+install_packages=(
+"distrobox"
+)
+
+remove_packages=(
+"toolbox"
+)
+
+rpm-ostree uninstall ${remove_packages[@]}
+rpm-ostree install ${install_packages[@]}
+
+cat <<EOF | sudo tee /etc/systemd/system/distrobox-upgrade.service > /dev/null
+[Unit]
+Description=distrobox-upgrade Automatic Update
+
+[Service]
+Type=simple
+ExecStart=distrobox-upgrade --all
+StandardOutput=null
+EOF
+
+cat <<EOF | sudo tee /etc/systemd/system/distrobox-upgrade.timere > /dev/null
+[Unit]
+Description=distrobox-upgrade Automatic Update Trigger
+
+[Timer]
+OnBootSec=1h
+OnUnitInactiveSec=1d
+
+[Install]
+WantedBy=timers.target
+EOF
+
+systemctl enable distrobox-upgrade.timer
