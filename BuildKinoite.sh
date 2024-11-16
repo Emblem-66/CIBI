67a49d6d9b6096d6f591bdca6767fbb2cd933ebc
Emblem-66 <143352594+Emblem-66@users.noreply.github.com>
2024-11-16 08:15:40 +0100

    Update BuildKinoite.sh

--- BuildKinoite.sh
+++ BuildKinoite.sh
@@ -2,27 +2,9 @@
 
 set -ouex pipefail
 
-#curl -s https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/system/autoupdate.sh | bash
-#curl -s https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/system/kernel.sh | bash
-#curl -s https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/system/mesa-git.sh | bash
-#curl -s https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/system/codecs.sh | bash
-#curl -s https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/system/fonts.sh | bash
-#curl -s https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/system/utilities.sh | bash
-#curl -s https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/system/tailscale.sh | bash
-#curl -s https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/system/just.sh | bash
-
-#curl -s https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/kinoite/debloat.sh | bash
-#curl -s https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/kinoite/packages.sh | bash
-
-#curl -s https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/application/steam.sh | bash
-#curl -s https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/application/bottles.sh | bash
-
-BASE_URL="https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts"
-
 SYS="https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/system"
 KN="https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/kinoite"
 APP="https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/application"
-
 
 # System scripts
 curl -s "${SYS}/autoupdate.sh" | bash
@@ -40,9 +22,7 @@ curl -s "${KN}/debloat.sh" | bash
 curl -s "${KN}/packages.sh" | bash
 
 # Application scripts
-#curl -s "${APP}/steam.sh" | bash
-#curl -s "${APP}/bottles.sh" | bash
-
-rpm -qa | sort
+curl -s "${APP}/steam.sh" | bash
+curl -s "${APP}/bottles.sh" | bash
 
 rpm -qa | sort
