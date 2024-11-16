92740363890a07cc860303a20711a28045a5c746
Emblem-66 <143352594+Emblem-66@users.noreply.github.com>
2024-11-16 15:30:20 +0100

    Update just.sh

--- scripts/system/just.sh
+++ scripts/system/just.sh
@@ -13,6 +13,18 @@ install_packages=(
 )
 rpm-ostree install --idempotent ${install_packages[@]}
 
-#cat <<EOF | sudo tee /etc/Justfile > /dev/null
-#justfile
-#EOF
+cat <<EOF | sudo tee /etc/Justfile > /dev/null
+
+flatpak-setup
+    curl -s https://raw.githubusercontent.com/Emblem-66/just-scripts/refs/heads/main/flatpak-setup.sh | bash
+
+flatpak-apps
+    curl -s https://raw.githubusercontent.com/Emblem-66/just-scripts/refs/heads/main/flatpak-APPS.sh | bash
+
+flatpak-kde-apps
+    curl -s https://raw.githubusercontent.com/Emblem-66/just-scripts/refs/heads/main/flatpak-GNOME.sh | bash
+
+flatpak-gnome-apps
+    curl -s https://raw.githubusercontent.com/Emblem-66/just-scripts/refs/heads/main/flatpak-KDE.sh | bash
+
+EOF
