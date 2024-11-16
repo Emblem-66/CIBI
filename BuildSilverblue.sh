393b2dc5321a3b747d539e269ab02ece677938c9
Emblem-66 <143352594+Emblem-66@users.noreply.github.com>
2024-11-16 08:15:59 +0100

    Update BuildSilverblue.sh

--- BuildSilverblue.sh
+++ BuildSilverblue.sh
@@ -2,26 +2,9 @@
 
 set -ouex pipefail
 
-#curl -s https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/system/autoupdate.sh | bash
-#curl -s https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/system/kernel.sh | bash
-#curl -s https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/system/mesa-git.sh | bash
-#curl -s https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/system/codecs.sh | bash
-#curl -s https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/system/fonts.sh | bash
-#curl -s https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/system/utilities.sh | bash
-#curl -s https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/system/just.sh | bash
-
-#curl -s https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/silverblue/debloat.sh | bash
-#curl -s https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/silverblue/packages.sh | bash
-
-#curl -s https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/application/steam.sh | bash
-#curl -s https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/application/bottles.sh | bash
-
-BASE_URL="https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts"
-
 SYS="https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/system"
 SB="https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/silverblue"
 APP="https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/application"
-
 
 # System scripts
 curl -s "${SYS}/autoupdate.sh" | bash
@@ -39,7 +22,7 @@ curl -s "${SB}/debloat.sh" | bash
 curl -s "${SB}/packages.sh" | bash
 
 # Application scripts
-#curl -s "${APP}/steam.sh" | bash
-#curl -s "${APP}/bottles.sh" | bash
+curl -s "${APP}/steam.sh" | bash
+curl -s "${APP}/bottles.sh" | bash
 
 rpm -qa | sort
