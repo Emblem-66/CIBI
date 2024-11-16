fe0d03488c8aad491eb7ec38e90bd5edaab7a6a6
Emblem-66 <143352594+Emblem-66@users.noreply.github.com>
2024-11-16 16:40:15 +0100

    Create debloat.sh

---
+++ scripts/miniblue/debloat.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+remove_packages=(
+"firefox"
+"firefox-langpacks"
+"gnome-classic-session"
+"gnome-shell-extension-window-list"
+"gnome-shell-extension-background-logo"
+"gnome-shell-extension-launch-new-instance"
+"gnome-shell-extension-apps-menu"
+"gnome-shell-extension-places-menu"
+"gnome-tour"
+"yelp"
+"yelp-libs"
+"yelp-xsl"
+)
+
+#rpm -q ${remove_packages[@]} || rpm-ostree uninstall ${remove_packages[@]}
+rpm-ostree install --idempotent --force-replacefiles ${remove_packages[@]}
