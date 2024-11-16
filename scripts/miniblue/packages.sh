c44e5f86ea173625917c66c0e3ca2dccc9e396c6
Emblem-66 <143352594+Emblem-66@users.noreply.github.com>
2024-11-16 16:41:27 +0100

    Create packages.sh

---
+++ scripts/miniblue/packages.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+rpmfusion=(
+    "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
+    "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
+)
+#rpm -q rpmfusion-free-release rpmfusion-nonfree-release || rpm-ostree install ${rpmfusion[@]}
+
+install_packages=(
+"adw-gtk3-theme"
+"gnome-shell-extension-caffeine"
+)
+
+install_applications=(
+"ffmpegthumbnailer"
+"gnome-tweaks"
+"clapper"
+"gnome-text-editor"
+"loupe"
+)
+#rpm -q ${install_packages[@]} || rpm-ostree install ${install_packages[@]}
+rpm-ostree install --idempotent --force-replacefiles ${install_packages[@]}
+#rpm-ostree install --idempotent --force-replacefiles ${install_applications[@]}
+
+git clone https://github.com/mukul29/legacy-theme-auto-switcher-gnome-extension.git /usr/share/gnome-shell/extensions/legacyschemeautoswitcher@joshimukul29.gmail.com
+
