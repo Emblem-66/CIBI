79745fae7cadc26f39db29a109adca460d4eaabc
Emblem-66 <143352594+Emblem-66@users.noreply.github.com>
2024-11-16 12:08:19 +0100

    Update prismlauncher.sh

--- scripts/application/prismlauncher.sh
+++ scripts/application/prismlauncher.sh
@@ -4,22 +4,18 @@ set -ouex pipefail
 
 curl -o /etc/yum.repos.d/_copr_prismlauncher.repo "https://copr.fedorainfracloud.org/coprs/g3tchoo/prismlauncher/repo/fedora-rawhide/g3tchoo-prismlauncher-fedora-rawhide.repo"
 
-rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-
-rpm-ostree install prismlauncher
-
 rpmfusion=(
     "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
     "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
 )
-rpm -q rpmfusion-free-release rpmfusion-nonfree-release || rpm-ostree install ${rpmfusion[@]}
+rpm-ostree install --idempotent --force-replacefiles ${rpmfusion[@]}
 
 install_packages=(
-    ""
+"prismlauncher"
 )
-rpm -q ${install_packages[@]} || rpm-ostree install ${install_packages[@]}
+rpm-ostree install --idempotent --force-replacefiles ${install_packages[@]}
 
 remove_packages=(
-    ""
+""
 )
-rpm -q ${remove_packages[@]} || rpm-ostree uninstall ${remove_packages[@]}
+rpm-ostree install --idempotent --force-replacefiles ${remove_packages[@]}
