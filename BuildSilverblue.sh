693770ccddf30f5d5e373e89abf1dd0d5bc0456d
Emblem-66 <143352594+Emblem-66@users.noreply.github.com>
2024-11-18 08:28:19 +0100

    Update BuildSilverblue.sh

--- BuildSilverblue.sh
+++ BuildSilverblue.sh
@@ -8,13 +8,13 @@ APP="https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/ap
 
 # System scripts
 curl -s "${SYS}/autoupdate.sh" | bash
-#curl -s "${SYS}/kernel.sh" | bash
-#curl -s "${SYS}/mesa-git.sh" | bash
-#curl -s "${SYS}/codecs.sh" | bash
+curl -s "${SYS}/kernel.sh" | bash
+curl -s "${SYS}/mesa-git.sh" | bash
+curl -s "${SYS}/codecs.sh" | bash
 curl -s "${SYS}/fonts.sh" | bash
 #curl -s "${SYS}/utilities.sh" | bash
 curl -s "${SYS}/tailscale.sh" | bash
-#curl -s "${SYS}/distrobox.sh" | bash
+curl -s "${SYS}/distrobox.sh" | bash
 curl -s "${SYS}/just.sh" | bash
 
 # Silverblue scripts
@@ -22,9 +22,9 @@ curl -s "${SB}/debloat.sh" | bash
 curl -s "${SB}/packages.sh" | bash
 
 # Application scripts
-#curl -s "${APP}/steam.sh" | bash
-#curl -s "${APP}/bottles.sh" | bash
-#curl -s "${APP}/heroic.sh" | bash
-#curl -s "${APP}/prismlauncher.sh" | bash
+curl -s "${APP}/steam.sh" | bash
+curl -s "${APP}/bottles.sh" | bash
+curl -s "${APP}/heroic.sh" | bash
+curl -s "${APP}/prismlauncher.sh" | bash
 
 rpm -qa | sort
