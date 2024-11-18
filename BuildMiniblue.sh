3f23026a788b27cc16d83e0e78bdf7ee138a8dde
Emblem-66 <143352594+Emblem-66@users.noreply.github.com>
2024-11-18 08:27:40 +0100

    Update BuildMiniblue.sh

--- BuildMiniblue.sh
+++ BuildMiniblue.sh
@@ -8,9 +8,6 @@ APP="https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/ap
 
 # System scripts
 curl -s "${SYS}/autoupdate.sh" | bash
-#curl -s "${SYS}/kernel.sh" | bash
-#curl -s "${SYS}/mesa-git.sh" | bash
-#curl -s "${SYS}/codecs.sh" | bash
 curl -s "${SYS}/fonts.sh" | bash
 #curl -s "${SYS}/utilities.sh" | bash
 curl -s "${SYS}/tailscale.sh" | bash
@@ -20,11 +17,5 @@ curl -s "${SYS}/just.sh" | bash
 # Miniblue scripts
 curl -s "${MB}/debloat.sh" | bash
 curl -s "${MB}/packages.sh" | bash
-
-# Application scripts
-#curl -s "${APP}/steam.sh" | bash
-#curl -s "${APP}/bottles.sh" | bash
-#curl -s "${APP}/heroic.sh" | bash
-#curl -s "${APP}/prismlauncher.sh" | bash
 
 rpm -qa | sort
