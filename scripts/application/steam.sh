3aca5654dab66d2be9f67ef989f20cdabae53e45
Emblem-66 <143352594+Emblem-66@users.noreply.github.com>
2024-11-30 00:31:39 +0100

    Update steam.sh

--- scripts/application/steam.sh
+++ scripts/application/steam.sh
@@ -6,7 +6,7 @@ rpmfusion=(
     "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
     "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
 )
-rpm -q rpmfusion-free-release rpmfusion-nonfree-release || rpm-ostree install ${rpmfusion[@]}
+#rpm -q rpmfusion-free-release rpmfusion-nonfree-release || rpm-ostree install ${rpmfusion[@]}
 
 install_packages=(
 "steam"
@@ -17,6 +17,24 @@ install_packages=(
 )
 #rpm -q ${install_packages[@]} || rpm-ostree install  --force-replacefiles ${install_packages[@]}
 
-rpm-ostree install --idempotent --force-replacefiles ${install_packages[@]}
+#rpm-ostree install --idempotent --force-replacefiles ${install_packages[@]}
 
 #sudo sed -i 's|Exec=/usr/bin/steam %U|Exec=gamemoderun /usr/bin/steam %U|' /etc/share/applications/steam.desktop
+
+
+
+
+rpmfusion=(
+    "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
+    "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
+)
+dnf install ${rpmfusion[@]}
+
+install_packages=(
+"steam"
+"goverlay"
+"mangohud"
+"gamemode"
+"vkBasalt"
+)
+dnf install ${install_packages[@]}
