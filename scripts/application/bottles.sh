7f74700f9b9ac42616db052b1ba8c0c40b32a6b6
Emblem-66 <143352594+Emblem-66@users.noreply.github.com>
2024-11-30 00:35:11 +0100

    Update bottles.sh

--- scripts/application/bottles.sh
+++ scripts/application/bottles.sh
@@ -2,13 +2,16 @@
 
 set -ouex pipefail
 
-curl -o /etc/yum.repos.d/_copr_bottles.repo "https://copr.fedorainfracloud.org/coprs/gui1ty/bottles/repo/fedora-rawhide/gui1ty-bottles-fedora-rawhide.repo"
+#curl -o /etc/yum.repos.d/_copr_bottles.repo "https://copr.fedorainfracloud.org/coprs/gui1ty/bottles/repo/fedora-rawhide/gui1ty-bottles-fedora-rawhide.repo"
+
+dnf copr enable -y gui1ty/bottles
 
 rpmfusion=(
     "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
     "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
 )
-rpm -q rpmfusion-free-release rpmfusion-nonfree-release || rpm-ostree install ${rpmfusion[@]}
+#rpm -q rpmfusion-free-release rpmfusion-nonfree-release || rpm-ostree 
+dnf install -y ${rpmfusion[@]}
 
 install_packages=(
 "bottles"
@@ -17,4 +20,6 @@ install_packages=(
 "gamemode"
 "vkBasalt"
 )
-rpm-ostree install --idempotent --force-replacefiles ${install_packages[@]}
+#rpm-ostree install --idempotent --force-replacefiles ${install_packages[@]}
+
+dnf install -y ${install_packages[@]}
