b81c3fc628b1bb42c20170cec6fdfc0a0dade4ad
Emblem-66 <143352594+Emblem-66@users.noreply.github.com>
2024-12-19 20:44:06 +0100

    Update BuildSilverblue.sh

--- BuildSilverblue.sh
+++ BuildSilverblue.sh
@@ -2,29 +2,29 @@
 
 set -ouex pipefail
 
-SYS="https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/system"
-SB="https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/silverblue"
-APP="https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/application"
+#SYS="https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/system"
+#SB="https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/silverblue"
+#APP="https://raw.githubusercontent.com/Emblem-66/CIBI/refs/heads/main/scripts/application"
 
 # System scripts
-curl -s "${SYS}/autoupdate.sh" | bash
-curl -s "${SYS}/kernel.sh" | bash
-curl -s "${SYS}/mesa-git.sh" | bash
-curl -s "${SYS}/codecs.sh" | bash
-curl -s "${SYS}/fonts.sh" | bash
-curl -s "${SYS}/utilities.sh" | bash
-curl -s "${SYS}/tailscale.sh" | bash
-curl -s "${SYS}/distrobox.sh" | bash
-curl -s "${SYS}/just.sh" | bash
+#curl -s "${SYS}/autoupdate.sh" | bash
+#curl -s "${SYS}/kernel.sh" | bash
+#curl -s "${SYS}/mesa-git.sh" | bash
+#curl -s "${SYS}/codecs.sh" | bash
+#curl -s "${SYS}/fonts.sh" | bash
+#curl -s "${SYS}/utilities.sh" | bash
+#curl -s "${SYS}/tailscale.sh" | bash
+#curl -s "${SYS}/distrobox.sh" | bash
+#curl -s "${SYS}/just.sh" | bash
 
 # Silverblue scripts
-curl -s "${SB}/debloat.sh" | bash
-curl -s "${SB}/packages.sh" | bash
+#curl -s "${SB}/debloat.sh" | bash
+#curl -s "${SB}/packages.sh" | bash
 
 # Application scripts
-curl -s "${APP}/steam.sh" | bash
-curl -s "${APP}/bottles.sh" | bash
-curl -s "${APP}/heroic.sh" | bash
+#curl -s "${APP}/steam.sh" | bash
+#curl -s "${APP}/bottles.sh" | bash
+#curl -s "${APP}/heroic.sh" | bash
 #curl -s "${APP}/prismlauncher.sh" | bash
 
 rpm -qa | sort
